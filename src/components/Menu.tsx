14e37861ac472c7d64da47f43bbc75c4003bb5b9
한준 <s22073@gsm.hs.kr>
2023-10-13 01:59:10 +0900

    update stuff language

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,6 +7,42 @@ interface MenuItem {
     price: number;
 }
 
+const translateStuff = (originalStuff: string) => {
+    // 값들을 쉼표를 기준으로 분할
+    const stuffArray = originalStuff.split(',');
+
+    // 각 값에 대해 번역 적용
+    const translatedStuffArray = stuffArray.map(item => {
+        switch (item.trim()) {
+            case 'dan':
+                return '단무지';
+            case 'che':
+                return '치즈';
+            case 'cam':
+                return '참치';
+            case 'dang':
+                return '당근';
+            case 'oi':
+                return '오이';
+            case 'ung':
+                return '우엉';
+            case 'mat':
+                return '맛살';
+            case 'don':
+                return '돈까스';
+            case 'egg':
+                return '계란';
+            case 'ham':
+                return '햄';
+            default:
+                return item;
+        }
+    });
+
+    // 번역된 값들을 쉼표로 다시 결합
+    return translatedStuffArray.join(', ');
+}
+
 const Menu = () => {
     const [menu, setMenu] = useState<MenuItem[]>([]);
     const URL = `http://39.116.14.193:8080/api/getOrder`;
@@ -26,20 +62,23 @@ const Menu = () => {
     return (
         <div>
             {reversedMenu.map((list, index) => {
+                const translatedStuff = translateStuff(list.stuff);
                 const formatDate = new Date(list.date).toLocaleString();
                 return (
-                    <li style={{ fontSize: '2rem', fontWeight: 'bold' }} key={index}>
-                        {Number(reversedMenu.length) - index}번째 주문
-                        <ul style={{ fontSize: '1.5rem' }}>
-                            재료 : {list.stuff}
-                        </ul>
-                        <ul style={{ fontSize: '1.5rem' }}>
-                            주문 시간 : {formatDate}<br />
-                        </ul>
-                        <ul style={{ fontSize: '1.5rem' }}>
-                            가격 : {list.price}원<br />
-                        </ul>
-                    </li>
+                    <>
+                        <li style={{ fontSize: '2rem', fontWeight: 'bold' }} key={index}>
+                            {Number(reversedMenu.length) - index}번째 주문
+                            <ul style={{ fontSize: '1.5rem' }}>
+                                재료 : {translatedStuff}
+                            </ul>
+                            <ul style={{ fontSize: '1.5rem' }}>
+                                주문 시간 : {formatDate}<br />
+                            </ul>
+                            <ul style={{ fontSize: '1.5rem' }}>
+                                가격 : {list.price}원<br />
+                            </ul>
+                        </li>
+                    </>
                 );
             })}
         </div>
